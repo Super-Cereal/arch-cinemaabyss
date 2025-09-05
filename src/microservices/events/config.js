7162a5b1f326c57eb0cabdf94201363692ab6fe4
Super-Cereal <bifrost.step@gmail.com>
2025-09-05 20:31:51 +0300

    fix

--- src/microservices/events/config.js
+++ src/microservices/events/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   config: {
-    PORT: process.env.PORT,
-    KAFKA_BROKERS: process.env.KAFKA_BROKERS,
+    PORT: process.env.PORT || 8080,
+    KAFKA_BROKERS: process.env.KAFKA_BROKERS || "kafka:9092",
   },
 };
--- src/microservices/proxy/config.js
+++ src/microservices/proxy/config.js
@@ -2,15 +2,11 @@ export const config = (() => {
   const PORT = Number(process.env.PORT) || 8000;
   const MONOLITH_URL = process.env.MONOLITH_URL;
   const MOVIES_SERVICE_URL = process.env.MOVIES_SERVICE_URL;
-  const EVENTS_SERVICE_URL = process.env.EVENTS_SERVICE_URL;
   const GRADUAL_MIGRATION = process.env.GRADUAL_MIGRATION === "true";
   const MOVIES_MIGRATION_PERCENT =
     Number(process.env.MOVIES_MIGRATION_PERCENT) || 0;
 
-  if (
-    !MONOLITH_URL ||
-    (GRADUAL_MIGRATION && (!MOVIES_SERVICE_URL || !EVENTS_SERVICE_URL))
-  ) {
+  if (!MONOLITH_URL || (GRADUAL_MIGRATION && !MOVIES_SERVICE_URL)) {
     throw new Error("Missing required environment variables");
   }
 
@@ -18,7 +14,6 @@ export const config = (() => {
     PORT,
     MONOLITH_URL,
     MOVIES_SERVICE_URL,
-    EVENTS_SERVICE_URL,
     GRADUAL_MIGRATION,
     MOVIES_MIGRATION_PERCENT,
   };
